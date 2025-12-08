81357579930869bc1158b48d626ac7dd12b1be2e
garima shrestha <garimashrestha20@gmail.com>
2025-12-08 10:26:14 +0545

    Grid View Code

--- lib/screens/gridview_screen.dart
+++ lib/screens/gridview_screen.dart
@@ -10,9 +10,53 @@ class GridViewScreen extends StatelessWidget {
         title: const Text('GridView'),
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
       ),
-      body: const Center(
-        child: Text('gridview', style: TextStyle(fontSize: 24)),
-      ),
+      body: GridView.count(
+        padding: const EdgeInsets.all(8),
+        crossAxisSpacing: 8,  // horizontal spacing
+        mainAxisSpacing: 8,  // vertical spacing
+        // crossAxisCount: 2,  // 2 columns
+        // children: [
+        //   Container(
+        //     alignment: Alignment.center,  // value lai container ko center ma lyauna
+        //     color: Colors.yellow,
+        //     child: const Text(
+        //       '1',
+        //       style: TextStyle(fontSize: 50, color: Colors.black),
+        //     ),
+        //   ),
+        //   Container(
+        //     alignment: Alignment.center,
+        //     color: Colors.amber,
+        //     child: const Text(
+        //       '2',
+        //       style: TextStyle(fontSize: 50, color: Colors.black),
+        //     ),
+        //   ),
+        // ],
+
+        // crossAxisCount: 3,  // 3 columns [3 by 3 matrix]
+        // children: [
+        //   ElevatedButton(onPressed: (){}, style: ElevatedButton.styleFrom(backgroundColor: Colors.amber), child: Text('1', style: TextStyle(fontSize: 25))),
+        //   ElevatedButton(onPressed: (){}, child: Text('2', style: TextStyle(fontSize: 25))),
+        //   ElevatedButton(onPressed: (){}, child: Text('3', style: TextStyle(fontSize: 25))),
+        // ],
+
+        crossAxisCount: 3,  // 3 columns [3 by 3 matrix]
+        children: [
+          for (int i = 1; i<=10; i++) ...{
+            SizedBox(
+              child: ElevatedButton(
+                  onPressed: (){},
+                  child: Text('$i', style: const TextStyle(fontSize: 50)),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.grey,
+                    foregroundColor: Colors.black,
+                  ),
+              ),
+            )
+          }
+        ],
+      )
     );
   }
 }
